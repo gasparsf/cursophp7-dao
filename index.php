593d381ec3b5ac43fe8c6bb3afbd607c7fab709a
Gaspar Ferreira <gaspar.sf@gmail.com>
2021-03-26 13:36:05 +0000

    Criação da Classe Usuario

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once("config.php");
 
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
+$root = new Usuario();
+
+$root->loadbyID(3);
+
+echo $root;
 ?>
