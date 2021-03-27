df77eff6885e3839395716e9bd7ef3aafe8d28f4
Gaspar Ferreira <gaspar.sf@gmail.com>
2021-03-27 14:16:46 +0000

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -58,6 +58,44 @@ class Usuario {
 		}
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM  tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM  tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.");
+		}
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
@@ -67,6 +105,7 @@ class Usuario {
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s"),
 		));
 	}
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -8,11 +8,24 @@ $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+
+
+// Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyID(3);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();  // É static, não precisa instanciar com 'new'
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("jo");	
+echo json_encode($search);
 */
 
-$root = new Usuario();
-
-$root->loadbyID(3);
-
-echo $root;
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$%123");
+echo $usuario;
 ?>
